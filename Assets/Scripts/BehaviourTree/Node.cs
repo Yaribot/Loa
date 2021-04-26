692587e9c22f9604639f85e570e996d38f294c6d
Yaribot <47753213+Yaribot@users.noreply.github.com>
2021-04-26 12:37:49 +0200

    Update Start Behaviour tree framework

---
+++ Assets/Scripts/BehaviourTree/Node.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Node 
+{
+    protected NodeState _nodeState;
+    public NodeState nodeState { get { return _nodeState; } }
+
+
+    public abstract NodeState Evaluate();
+}
+
+public enum NodeState
+{
+    RUNNING, SUCCCESS, FAILURE,
+}
---
+++ Assets/Scripts/BehaviourTree/Sequence.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sequence : Node
+{
+    protected List<Node> nodes = new List<Node>();
+
+    public Sequence(List<Node> nodes)
+    {
+        this.nodes = nodes;
+    }
+    public override NodeState Evaluate()
+    {
+        bool isAnyNodeRunning = false;
+        foreach (var node in nodes)
+        {
+            switch (node.Evaluate())
+            {
+                case NodeState.RUNNING:
+                    isAnyNodeRunning = true;
+                    break;
+                case NodeState.SUCCCESS:
+                    break;
+                case NodeState.FAILURE:
+                    _nodeState = NodeState.FAILURE;
+                    return _nodeState;
+                default:
+                    break;
+            }
+        }
+
+        _nodeState = isAnyNodeRunning ? NodeState.RUNNING : NodeState.SUCCCESS;
+        return _nodeState;
+    }
+}
